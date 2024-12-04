74865e58e22ac0c2666589b125315ceabe1cc077
surbhigoel555 <surbhigoel555@gmail.com>
2024-12-04 18:50:40 +0530

    Post Card Added

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import Logo from "../assets/logo.png";
 
 const PostCard = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e dark:bg-gray-800 dark:border-gray-700">
+      <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+          Very easy this was to integrate
+        </h3>
+        <p className="my-4">If you care for your time, I hands down would go with this."</p>
+      </blockquote>
+      <figcaption className="flex items-center justify-center ">
+        <img className="rounded-full w-9 h-9" src={Logo} alt="img" />
+        <div className="space-y-0.5 font-medium dark:text-white text-left rtl:text-right ms-3">
+          <div>
+            Bonnie Green
+            <i className="bi bi-trash3"></i>
+          </div>
+          <div className="text-sm text-gray-500 dark:text-gray-400 ">Date</div>
+        </div>
+      </figcaption>
+    </figure>
+  );
+};
 
-export default PostCard
+export default PostCard;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import PostCard from "../components/PostCard";
 
 const HomePage = () => {
   return (
-    <div>
-      HomePage
-    </div>
-  )
-}
+    <section>
+      <div className="grid mb-8 border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2 bg-white dark:bg-gray-800">
+        <PostCard />
+      </div>
+    </section>
+  );
+};
 
-export default HomePage
+export default HomePage;
