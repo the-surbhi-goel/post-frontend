63c8becdfc23e535f373c3b4de15ed8cefbc4a1e
surbhigoel555 <surbhigoel555@gmail.com>
2024-12-04 18:37:19 +0530

    Footer Added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer className="bg-white shadow dark:bg-gray-800">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © {new Date().getFullYear()}{" "}
+          <a href="/" className="hover:underline">
+            Post
+          </a>
+          . All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
